6771613cbd0d6994fa124fab048db53dbe665855
beigirad <f.beigirad@gmail.com>
2019-02-05 21:18:17 +0330

    added splashScreen

---
+++ app/src/main/java/ir/beigirad/app/SplashActivity.kt
@@ -0,0 +1,18 @@
+package ir.beigirad.app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+
+}
