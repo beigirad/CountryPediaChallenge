899d24d695db4fcdf8dd27467df1de5f61c504f1
beigirad <f.beigirad@gmail.com>
2019-02-05 10:20:01 +0330

    added domain module

---
+++ domain/build.gradle.kts
@@ -0,0 +1,12 @@
+import ir.beigiead.dependencies.*
+
+plugins {
+    id("kotlin")
+    id("kotlin-kapt")
+}
+
+dependencies {
+    implementation(Libraries.rxkotlin)
+    implementation(Libraries.dagger)
+    kapt(Libraries.daggerCompiler)
+}
