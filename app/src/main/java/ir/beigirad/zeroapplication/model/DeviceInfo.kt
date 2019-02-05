cda8c3d8cd9e577752764c9f330274aa62309871
beigirad <f.beigirad@gmail.com>
2019-02-05 14:17:25 +0330

    remove unused files from Zero

--- app/src/main/java/ir/beigirad/zeroapplication/model/DeviceInfo.kt
+++ app/src/main/java/ir/beigirad/zeroapplication/model/DeviceInfo.kt
@@ -1,24 +1,17 @@
 package ir.beigirad.zeroapplication.model
 
-import com.google.gson.annotations.SerializedName
 
 data class DeviceInfo(
 
-        @SerializedName("manufacture")
         var manufacture: String? = null,
 
-        @SerializedName("model")
         var model: String? = null,
 
-        @SerializedName("sdk")
         var sdk: Int = 0,
 
-        @SerializedName("app_version_name")
         var appVersionName: String? = null,
 
-        @SerializedName("app_version_code")
         var appVersionCode: Int,
 
-        @SerializedName("firebase_token")
         var firebaseToken: String? = null
 )
