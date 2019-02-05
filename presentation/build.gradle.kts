213dd2a4b15f5c3fd0bf87c5e43d0005d492fbbd
beigirad <f.beigirad@gmail.com>
2019-02-05 13:38:23 +0330

    setting up presentation

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,37 @@
+import ir.beigiead.dependencies.*
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(Versions.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(Versions.minSdk)
+        targetSdkVersion(Versions.targetSdk)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+        getByName("debug") {
+        }
+    }
+}
+
+dependencies {
+    implementation(Libraries.rxkotlin)
+    implementation(Libraries.dagger)
+    kapt(Libraries.daggerCompiler)
+
+    implementation(Libraries.lifecucleRuntime)
+    implementation(Libraries.lifecycleExtensions)
+    kapt(Libraries.lifecycleCompiler)
+
+    implementation(project(Modules.domain))
+}
